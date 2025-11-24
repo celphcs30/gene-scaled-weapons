ed286f0c7bf272ca0032a14634b948352afc7249
celphcs30 <69301528+celphcs30@users.noreply.github.com>
2025-11-24 22:03:26 +0000

    Initial commit

---
+++ GeneScaledWeapons/Source/GeneScaledWeapons/ModExt_SkipGeneWeaponScale.cs
@@ -0,0 +1,10 @@
+using Verse;
+
+namespace GeneScaledWeapons
+{
+    public class ModExt_SkipGeneWeaponScale : DefModExtension
+    {
+        public bool skip = true; // just having the extension means skip
+    }
+}
+
